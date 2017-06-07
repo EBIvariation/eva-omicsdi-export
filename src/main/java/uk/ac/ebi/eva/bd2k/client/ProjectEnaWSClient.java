dcc076a35b00ee6984b31d73f80b396c16ba2590
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-06-07 18:57:09 +0100

    Changed the way the first publication date is obtained from an ENA project

--- src/main/java/uk/ac/ebi/eva/bd2k/client/ProjectEnaWSClient.java
+++ src/main/java/uk/ac/ebi/eva/bd2k/client/ProjectEnaWSClient.java
@@ -17,12 +17,11 @@ package uk.ac.ebi.eva.bd2k.client;
 
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-import uk.ac.ebi.ena.sra.xml.AttributeType;
 import uk.ac.ebi.ena.sra.xml.ProjectType;
 
 import uk.ac.ebi.eva.bd2k.model.EnaProject;
 
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 
 /**
@@ -36,11 +35,14 @@ public class ProjectEnaWSClient implements ProjectClient {
 
     private ProjectType enaProjectType;
 
+    private SimpleDateFormat simpleDateFormat;
+
     public ProjectEnaWSClient(String projectServiceUrl, RestTemplate restTemplate) {
         this.projectServiceUrl = projectServiceUrl;
         this.restTemplate = restTemplate;
         HttpMessageConverter<ProjectType> messageConverter = new ProjectHttpMessageConverter();
         this.restTemplate.setMessageConverters(Collections.singletonList(messageConverter));
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
     }
 
     @Override
@@ -50,9 +52,7 @@ public class ProjectEnaWSClient implements ProjectClient {
     }
 
     private String getPublicationDate(ProjectType project) {
-        AttributeType[] attributes = project.getPROJECTATTRIBUTES().getPROJECTATTRIBUTEArray();
-        return Arrays.stream(attributes).filter(a -> a.getTAG().equals("ENA-FIRST-PUBLIC"))
-                     .map(AttributeType::getVALUE).findFirst().get();
+        return simpleDateFormat.format(project.getFirstPublic().getTime());
     }
 
     public ProjectType getEnaProjectType() {
